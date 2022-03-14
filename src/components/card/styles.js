fb7f2b8f7db491c6e87dd2978b6b22e5083d0a04
miru-porto <portocarreromiranda@gmail.com>
2022-03-14 20:13:59 -0300

    agregando input

--- src/components/card/styles.js
+++ src/components/card/styles.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native'
-
+import { colors, margin } from '../../constants/theme';
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -8,9 +8,9 @@ export const styles = StyleSheet.create({
         flex: 1,
         flexGrow: 0.4,
         justifyContent: 'center',
-        marginTop: 20,
+        marginTop: margin.medium,
         marginHorizontal: 50,
-        shadowColor: "#000",
+        shadowColor: colors.shadowColor,
         shadowOffset: {
             width: 0,
             height: 1,
@@ -18,5 +18,6 @@ export const styles = StyleSheet.create({
         shadowOpacity: 0.20,
         shadowRadius: 1.41,
         elevation: 2,
+        backgroundColor: colors.primaryBackground,
     },
 });
