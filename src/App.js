d677d5596a4e1c74da301de4cc5126d21722dcf1
miru-porto <portocarreromiranda@gmail.com>
2022-03-14 22:13:41 -0300

    agregando game-screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
+  Platform
 } from 'react-native';
 import StartGameScreen from './screens/start-game-screen';
+import GameScreen from './screens/game-screen';
 import { styles } from './styles';
 
+
 const App = () => {
+  const [userNumber, setUserNumber] = useState();
+
+  const handleStartGame = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  }
+
+  let content = <StartGameScreen onStartGame={handleStartGame}/>
+
+  if(userNumber) {
+    content = <GameScreen userOptions={userNumber} />
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <StartGameScreen />
+        {content}
     </SafeAreaView>
   );
 };
