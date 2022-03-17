eb4afb6820fefe522a67a75c403575ee68e25437
miru-porto <portocarreromiranda@gmail.com>
2022-03-16 21:47:42 -0300

    agregando pantalla de game over

--- src/screens/game-screen/index.js
+++ src/screens/game-screen/index.js
@@ -1,30 +1,57 @@
-import React, { useState } from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { useState, useRef, useEffect, useCallback } from 'react'
+import { View, Text, Button, Alert } from 'react-native'
 import { styles } from './styles'
 import Card from '../../components/card'
 import Header from '../../components/header'
-const generateRandomBetween = (min, max, exclude) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    const rndNum = Math.floor(Math.random() * (max - min)) + min;
-    if (rndNum === exclude) {
-        return generateRandomBetween(min, max, exclude);
-    }
-    else return rndNum;
-}
+import useOrientation from '../../hooks/use-orientation'
 
-const GameScreen = ({ userOptions }) => {
+const GameScreen = ({ userOptions, onGameOver }) => {
+    const orientation = useOrientation()
+    const generateRandomBetween = useCallback((min, max, exclude) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        const rndNum = Math.floor(Math.random() * (max - min)) + min;
+        if (rndNum === exclude) {
+            return generateRandomBetween(min, max, exclude);
+        }
+        else return rndNum;
+    }, [currentHigh, currentLow, currentGuess, userOptions])
+
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, userOptions))
+    const [rounds, setRounds] = useState(0)
+    const currentLow = useRef(1)
+    const currentHigh = useRef(100)
+
+    useEffect(() => {
+        if(currentGuess === userOptions) onGameOver(rounds)
+    }, [currentGuess, userOptions, onGameOver])
+
+    const handlerNextGuess = (direction) => {
+        if(
+            (direction === 'lower' && currentGuess < userOptions) ||
+            (direction === 'greater' && currentGuess > userOptions)
+        ) {
+            Alert.alert('No mientas...', 'tu sabes que no es verdad', [{text: '¡Disculpa!'}, {text: 'Cancelar'}])
+        }
+        if(direction === 'lower') {
+            currentHigh.current = currentGuess
+        } else {
+            currentLow.current = currentGuess
+        }
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
+        setCurrentGuess(nextNumber)
+        setRounds(current => current +1)
+    }
 
     return (
         <View style={styles.container}>
             <Header title='Juego Iniciado' />
-            <Card style={styles.cardContainer}>
+            <Card style={orientation?.isPortrait ? styles.cardContainer : styles.cardContainerLandscape}>
                 <Text style={styles.cardTitle}>La suposición del oponentes</Text>
                 <Text style={styles.confirmedText}>{currentGuess}</Text>
                 <View style={styles.buttonsContainer}>
-                    <Button title='Menor' onPress={() => null} color='#52528C'/>
-                    <Button title='Mayor' onPress={() => null} color='#52528C'/>
+                    <Button title='Menor' onPress={() => handlerNextGuess('lower')} color='#52528C'/>
+                    <Button title='Mayor' onPress={() => handlerNextGuess('greater')} color='#52528C'/>
                 </View>            
             </Card>
         </View>
--- src/screens/game-screen/styles.js
+++ src/screens/game-screen/styles.js
@@ -1,13 +1,17 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
 import { fontSize, colors, margin } from '../../constants/theme'
+
+const { width, height } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1
     },
     cardContainer: {
-        flex: 1,
-        flexGrow: 0.3,
+        height: height * 0.35,
+    },
+    cardContainerLandscape: {
+        height: height * 0.5,
     },
     confirmedText: {
         fontSize: fontSize.large,
